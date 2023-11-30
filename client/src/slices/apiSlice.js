22ea81e25685db6ee72c95bbb162db8ad6f13d2f
Peter-JC-Wu <peterjcwu@gmail.com>
2023-11-30 13:57:51 -0500

    jwt testing

--- client/src/slices/apiSlice.js
+++ client/src/slices/apiSlice.js
@@ -3,11 +3,10 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // production: process.env.REACT_APP_BACKEND_API_URL
-// dev: http://localhost:6000
+// dev: http://localhost:5001
 // baseUrl is an empty string because of using a proxy
 const baseQuery = fetchBaseQuery({ 
-  // baseUrl: process.env.REACT_APP_BACKEND_API_URL,
-  baseUrl: "http://localhost:5001"
+  baseUrl: process.env.REACT_APP_BACKEND_API_URL,
 });
 
 export const apiSlice = createApi({
